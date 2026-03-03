d17c50ec679fe60ede6b8a723b7d1a692c33a521
AnnapooraniKadhiravan <86883608+AnnapooraniKadhiravan@users.noreply.github.com>
2026-03-03 13:48:41 +0530

    Add files via upload

---
+++ MyCharRunner.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class MyCharRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyChar myChar = new MyChar('A');
+		System.out.println(myChar.isVowel());
+		System.out.println(myChar.isDigit());
+		System.out.println(myChar.isAlphabet());
+		System.out.println(myChar.isConsonant());
+		MyChar.printLowerCaseAlphabets();
+		MyChar.printUpperCaseAlphabets();
+
+	}
+
+}
